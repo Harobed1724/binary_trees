5f22ad19b21bdd301387ff55aecb6113a551dc4b
Caleb Makafui <52793199+KojoMarko@users.noreply.github.com>
2022-08-26 00:41:58 +0000

    Extracts the root node of a Max Binary Heap

--- 133-heap_extract.c
+++ 133-heap_extract.c
@@ -1,10 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * heap_extract - pending.
- * @root: pending.
- * Return: pending.
- */
+* heap_extract - extracts the root node of a Max Binary Heap
+* @root: double pointer to the root node of heap
+* Return: value stored in the root node
+*/
 int heap_extract(heap_t **root)
 {
 	if (!root || !*root)
